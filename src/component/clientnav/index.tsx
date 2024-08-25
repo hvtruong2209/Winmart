2e626d5693f0f63c885262351f3df3e76fbec151
Ho Van Truong <truong.ho@heatmob.net>
2024-08-26 00:15:45 +0700

    init nav

--- src/component/clientnav/index.tsx
+++ src/component/clientnav/index.tsx
@@ -1,11 +1,14 @@
 import logo from "assets/images/logowhite.svg";
 import { TextInput } from "component/textfield";
+import "./index.scss";
 export const ClientNav = (props: any) => {
   return (
     <div className="clientnav">
       <div className="main-container">
         <img src={logo} style={{ width: 190 }}></img>
-        <TextInput />
+        <input className="search-input" />
+        <div>Giỏ hàng</div>
+        <div>Hội viên</div>
       </div>
     </div>
   );
