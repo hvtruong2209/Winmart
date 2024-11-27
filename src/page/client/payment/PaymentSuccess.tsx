bbcbf9d9af05d54529bd57e63f494cec9b91cfc9
Ho Van Truong <truong.ho@heatmob.net>
2024-11-27 21:59:39 +0700

    fix payment

--- src/page/client/payment/PaymentSuccess.tsx
+++ src/page/client/payment/PaymentSuccess.tsx
@@ -1,33 +1,17 @@
-import { CheckoutService } from "api/Checkout";
-import { useEffect } from "react";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { getFormatCurrencyVND } from "Utils/Image";
 export const PaymentSuccess = () => {
   const navigate = useNavigate();
   const getQueryParams = () => {
     const query = new URLSearchParams(window.location.search);
     return {
-      vnpAmount: query.get("vnp_Amount"),
-      vnpResponseCode: query.get("vnp_ResponseCode"),
-      vnpTransactionStatus: query.get("vnp_TransactionStatus"),
-      orderId: query.get("vnp_OrderInfo")?.split("-")[0],
+      amount: query.get("amount"),
     };
   };
 
-  const { vnpAmount, vnpResponseCode, vnpTransactionStatus, orderId } = getQueryParams();
-
-  const handlePaymentStatus = async () => {
-    let isSuccess = false;
-    if (vnpResponseCode === "00" && vnpTransactionStatus === "00") {
-      isSuccess = true;
-    }
-    const res = await CheckoutService.getCheckoutCallback(orderId!, isSuccess);
-  };
-
-  useEffect(() => {
-    handlePaymentStatus();
-  }, []);
+  const { amount } = getQueryParams();
 
   return (
     <div className="payment-success">
@@ -35,8 +19,7 @@ export const PaymentSuccess = () => {
         <CheckCircleIcon style={{ color: "#51cf66", height: 100, width: 100 }} />
         <h1>Đặt hàng thành công</h1>
         <p>
-          {`Bạn đã đặt hàng thành công đơn hàng trị giá 300.000 VND.
-          Sau khi Shop xác nhận đơn hàng, sản phẩm sẽ được giao đến địa chỉ H6 Hồ Chí Minh Hồ Chí Minh tại Dự kiến hiện tại Thứ năm 6 (23/04/2021). 
+          {`Bạn đã đặt hàng thành công đơn hàng trị giá ${getFormatCurrencyVND(Number(amount))}.
           Bạn có thể theo dõi đơn hàng tại mục Thông tin tài khoản > Theo dõi đơn hàng hoặc nhấp vào mục Chi tiết đơn hàng bên dưới.
           SHmarket rất vui được phục vụ bạn!`}
         </p>
